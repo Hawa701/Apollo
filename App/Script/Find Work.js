aea70f7a89e5566f778410c4a7ff1307149ba810
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-04-05 12:59:52 +0300

    Update on the Find work page
    *Styling for "tags" is now done by id (for making it easier and efficient)
    *Apply Job Page can now be seen by clicking on either the job title or description
    *Bookmark button changes color when clicked
    *JS file added

---
+++ App/Script/Find Work.js
@@ -0,0 +1,21 @@
+//toggling the apply job page
+function openApplyJob() {
+  document.getElementById("apply-job").style.width = "65%";
+  document.getElementById("apply-job").style.left = "unset";
+}
+
+function closeApplyJob() {
+  document.getElementById("apply-job").style.width = "0%";
+  document.getElementById("apply-job").style.left = "100%";
+}
+
+//saving a job
+function saveJob(iconID) {
+  if (document.getElementById(iconID).className == "fa-regular fa-bookmark") {
+    document.getElementById(iconID).className = "fa-solid fa-bookmark";
+    document.getElementById(iconID).style.color = "var(--main-color)";
+  } else {
+    document.getElementById(iconID).className = "fa-regular fa-bookmark";
+    document.getElementById(iconID).style.color = "var(--main-text-color)";
+  }
+}
