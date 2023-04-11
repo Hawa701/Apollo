00dcf93fbd4853ef5cf247bcef78825f08dd794e
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-04-11 06:11:45 +0300

    Updates on Find work page
    *Responsive
    *Filter button added
    *Save job button (inside the apply page) works

--- App/Script/Find Work.js
+++ App/Script/Find Work.js
@@ -1,6 +1,6 @@
 //toggling the apply job page
-function openApplyJob() {
-  document.getElementById("apply-job").style.width = "67%";
+function openApplyJob(x) {
+  document.getElementById("apply-job").style.width = x;
   document.getElementById("apply-job").style.left = "unset";
 }
 
@@ -9,8 +9,19 @@ function closeApplyJob() {
   document.getElementById("apply-job").style.left = "100%";
 }
 
-//saving a job
-function saveJob(iconID) {
+//toggling the filter btn
+function toggleFilter(iconID) {
+  if (document.getElementById(iconID).className == "fa-solid fa-filter") {
+    document.getElementById(iconID).className = "fa-solid fa-square-minus";
+    document.getElementById("filter").style.display = "inline-block";
+  } else {
+    document.getElementById(iconID).className = "fa-solid fa-filter";
+    document.getElementById("filter").style.display = "none";
+  }
+}
+
+//saving a job (icon)
+function saveJobIcon(iconID) {
   if (document.getElementById(iconID).className == "fa-regular fa-bookmark") {
     document.getElementById(iconID).className = "fa-solid fa-bookmark";
     document.getElementById(iconID).style.color = "var(--main-color)";
@@ -18,4 +29,18 @@ function saveJob(iconID) {
     document.getElementById(iconID).className = "fa-regular fa-bookmark";
     document.getElementById(iconID).style.color = "var(--main-text-color)";
   }
+}
+
+//saving a job (in apply page)
+function saveJobBtn(btnID) {
+  if (document.getElementById(btnID).innerText == "Save Job") {
+    document.getElementById(btnID).innerText = "Job Saved";
+  } else {
+    document.getElementById(btnID).innerText = "Save Job";
+  }
+}
+
+//Clearing the filter section
+function clear() {
+  //document.getElementsById("level") ... = "checked";
 }
