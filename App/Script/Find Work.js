af38a46f4eb5efbb0f2b9d66f1d29edd809913f8
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-04-23 09:11:41 +0300

    Added the Find work page and How it works page
    * Find work page can uses an Array (for the time being)
    * Its responsive
    * Searching works
    * Bookmarking a page works
    * Apply Page works
    * Clearing the chosen Filter works
    
    How it Works page is not 100% done, but most of the things are done

---
+++ App/Script/Find Work.js
@@ -0,0 +1,324 @@
+const jobContainer = document.getElementById("job-container"); //for targetting job-container
+const applyJob = document.getElementById("apply-job"); //for toggling apply job
+const applyJobInfo = document.getElementById("all-job-information"); //for filling job info in the apply job page
+const filterIcon = document.getElementById("filter"); //for filter button
+const clearBtn = document.getElementById("clear"); //for clear button
+const checkBoxes = document.querySelectorAll("[type=checkbox]"); //for targetting each checkbox
+const searchBtn = document.getElementById("search"); //for search button
+const resultLabel = document.getElementById("result"); //for result label
+
+//job array
+const jobArray = [
+  {
+    index: 0,
+    jobId: 1,
+    title: "Woocommerce Custom Checkout",
+    payment: 5000,
+    experience: "Intermediate",
+    date: "4/19/2023",
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, ex ut. Minima natus blanditiis incidunt animi, assumenda repudiandae facere debitis a distinctio voluptate quibusdamodio itaque eveniet similique cupiditate magni! Provident expedita, nemo ad possimus, quod incidunt vel fugit nobis earum repellendus nulla ullam necessitatibus fuga voluptates laborum dolorem, vitae quasi delectus facere molestias maxime minus beatae perspiciatis est. Blanditiis quia rerum nemo repudiandae aut ea et porro, excepturi soluta? <br /><br /><a href="#">https://dummy.link</a><br /><a href="#">https://dummylink</a><br /><a href="#">https://dummy.link</a>',
+    tag: [],
+    proposals: "Less than 5",
+    token: "6",
+  },
+  {
+    index: 1,
+    jobId: 2,
+    title: "Marketing Coordinator",
+    payment: 10000,
+    experience: "Expert",
+    date: "4/19/2023",
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, ex ut. Minima natus blanditiis incidunt animi, assumenda repudiandae facere debitis a distinctio voluptate quibusdamodio itaque eveniet similique cupiditate magni! Provident expedita, nemo ad possimus, quod incidunt vel fugit nobis earum repellendus nulla ullam necessitatibus fuga voluptates laborum dolorem, vitae quasi delectus facere molestias maxime minus beatae perspiciatis est. Blanditiis quia rerum nemo repudiandae aut ea et porro, excepturi soluta? <br /><br /> <a href="#">https://dummy.link</a><br /><a href="#">https://dummylink</a><br /><br /> Dolor sit amet consectetur adipisicing elit. Quas, ex ut. Minima natus blanditiis incidunt animi, assumenda repudiandae facere debitis a distinctio voluptate quibusdamodio itaque eveniet similique cupiditate magni! Provident expedita, nemo ad possimus, quod incidunt vel fugit nobis earum repellendus nulla ullam necessitatibus fuga voluptates laborum dolorem, vitae quasi delectus facere molestias maxime minus beatae perspiciatis est. Blanditiis quia rerum nemo repudiandae aut ea et porro, excepturi soluta?',
+    tag: [],
+    proposals: "5 to 10",
+    token: "10",
+  },
+  {
+    index: 2,
+    jobId: 3,
+    title: "Web Designer",
+    payment: 15000,
+    experience: "Enrty Level",
+    date: "4/19/2023",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, ex ut. Minima natus blanditiis incidunt animi, assumenda repudiandae facere debitis a distinctio voluptate quibusdamodio itaque eveniet similique cupiditate magni! Provident expedita, nemo ad possimus, quod incidunt vel fugit nobis earum repellendus nulla ullam necessitatibus fuga voluptates laborum dolorem, vitae quasi delectus facere molestias maxime minus beatae perspiciatis est. Blanditiis quia rerum nemo repudiandae aut ea et porro, excepturi soluta?",
+    tag: [],
+    proposals: "10 to 15",
+    token: "4",
+  },
+  {
+    index: 3,
+    jobId: 4,
+    title: "Project Manager",
+    payment: 20000,
+    experience: "Intermediate",
+    date: "4/19/2023",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, ex ut. Minima natus blanditiis incidunt animi, assumenda repudiandae facere debitis a distinctio voluptate quibusdamodio itaque eveniet similique cupiditate magni! Provident expedita, nemo ad possimus, quod incidunt vel fugit nobis earum repellendus nulla ullam necessitatibus fuga voluptates laborum dolorem, vitae quasi delectus facere molestias maxime minus beatae perspiciatis est. Blanditiis quia rerum nemo repudiandae aut ea et porro, excepturi soluta?",
+    tag: [],
+    proposals: "15+",
+    token: "5",
+  },
+];
+
+//tag array
+const tagArray = [
+  { tagid: 1, jobId: 1, tagName: "PHP" },
+  { tagid: 2, jobId: 1, tagName: "CSS" },
+  { tagid: 3, jobId: 1, tagName: "JS" },
+  { tagid: 4, jobId: 1, tagName: "HTML" },
+
+  { tagid: 5, jobId: 2, tagName: "PHP" },
+  { tagid: 6, jobId: 2, tagName: "CSS" },
+  { tagid: 7, jobId: 2, tagName: "HTML" },
+
+  { tagid: 8, jobId: 3, tagName: "Angular" },
+  { tagid: 9, jobId: 3, tagName: "React" },
+  { tagid: 10, jobId: 3, tagName: "Vue" },
+  { tagid: 11, jobId: 3, tagName: "JS" },
+
+  { tagid: 12, jobId: 4, tagName: "CSS" },
+  { tagid: 13, jobId: 4, tagName: "PHP" },
+];
+
+//loading the jobs in the container
+const jobLoader = () => {
+  jobContainer.innerHTML = "";
+
+  //filling the tags
+  for (var i = 0; i < jobArray.length; i++) {
+    for (let j = 0; j < tagArray.length; j++) {
+      if (jobArray[i].jobId == tagArray[j].jobId) {
+        jobArray[i].tag.push(tagArray[j].tagName);
+      }
+    }
+  }
+
+  //loading the job
+  for (var i = 0, suffix = 1; i < jobArray.length; i++, suffix++) {
+    jobContainer.innerHTML +=
+      `<div class="jobs" id="${jobArray[i].id}">
+              <div class="title-block">
+                <h3 onclick="openApplyJob('68%', ${jobArray[i].index})">
+                  ${jobArray[i].title}
+                </h3>
+                <div class="bookmark" onclick="saveJobIcon('bookmark-icon${suffix}')">
+                  <i class="fa-regular fa-bookmark" id="bookmark-icon${suffix}"></i>
+                </div>
+              </div>
+
+              <div class="job-info">
+                <span>
+                  Payment:
+                  <span>${jobArray[i].payment}ETB</span>
+                  - <span>${jobArray[i].experience}</span>
+                  - Est. Time:
+                  <span>2 to 5 months</span>
+                  - Posted -
+                  <span>${jobArray[i].date}</span>
+                </span>
+              </div>
+
+              <div class="description" onclick="openApplyJob('68%', ${jobArray[i].index})">
+                <p>
+                  ${jobArray[i].description}
+                </p>
+              </div> ` +
+      getTag(i) +
+      `<div class="apply-info">
+                <div class="text">Proposals: <span>${jobArray[i].proposals}</span></div>
+                <div class="text">Number of tokens: <span>${jobArray[i].token}</span></div>
+              </div>
+
+              <hr />
+            </div>`;
+  }
+};
+
+//Function that loads that tags in a container
+function getTag(i) {
+  let tags = '<div id="tags">';
+  for (let j = 0; j < jobArray[i].tag.length; j++) {
+    tags += `<span class="tag">${jobArray[i].tag[j]}</span>`;
+  }
+  tags += `</div>`;
+  return tags;
+}
+
+jobLoader();
+
+//loading the apply job data
+const applyJobInfoLoader = (index) => {
+  applyJobInfo.innerHTML = "";
+  applyJobInfo.innerHTML =
+    `<div class="info one">
+              <h3>${jobArray[index].title}</h3>
+              <h4>Front-End Development</h4>
+              <p>${jobArray[index].date}</p>
+            </div>
+            <div class="info two">
+              <p>${jobArray[index].description}</p>
+            </div>
+            <div class="info three">
+              <button id="ap-btn" class="btn apply">Apply Now</button>
+              <button
+                id="sv-btn"
+                class="btn save"
+                onclick="saveJobBtn('sv-btn')"
+              >
+                Save Job
+              </button>
+            </div>
+            <div class="info four">
+              <div class="box price">
+                <h4>${jobArray[index].payment} ETB</h4>
+                <span>Fixed Price</span>
+              </div>
+              <div class="box ex-level">
+                <h4>${jobArray[index].experience}</h4>
+                <span
+                  >Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Fuga, maxime.</span
+                >
+              </div>
+            </div>
+            <div class="info five">
+              <h4>Project type</h4>
+              <span>One time project</span>
+            </div>
+            <div class="info six">
+              <h4>About the client</h4>
+              <p>Posted Jobs: <span>8</span></p>
+            </div>
+            <div class="info seven">
+              <h4>Skills and Expertise</h4>
+              ` +
+    getTag(index) +
+    `
+            </div>
+            <div class="info eight">
+              <h4>Job link</h4>
+            </div>
+            <div class="info nine">
+              <h4>Activity on this job</h4>
+              <p>Proposals: <span>${jobArray[index].proposals}</span></p>
+              <p>Interviews: <span>10</span></p>
+            </div>`;
+};
+
+//toggling the apply job page
+
+function openApplyJob(wid, id) {
+  applyJobInfoLoader(id);
+  applyJob.style.width = wid;
+  applyJob.style.left = "unset";
+}
+
+function closeApplyJob() {
+  applyJob.style.left = "100%";
+  applyJob.style.width = "0%";
+}
+
+//saving a job (icon)
+function saveJobIcon(iconID) {
+  if (document.getElementById(iconID).className == "fa-regular fa-bookmark") {
+    document.getElementById(iconID).className = "fa-solid fa-bookmark";
+    document.getElementById(iconID).style.color = "var(--main-color)";
+  } else {
+    document.getElementById(iconID).className = "fa-regular fa-bookmark";
+    document.getElementById(iconID).style.color = "var(--main-text-color)";
+  }
+}
+
+//saving a job (in apply page)
+function saveJobBtn(btnID) {
+  if (document.getElementById(btnID).innerText == "Save Job") {
+    document.getElementById(btnID).innerText = "Job Saved";
+  } else {
+    document.getElementById(btnID).innerText = "Save Job";
+  }
+}
+
+//toggling the filter btn
+function toggleFilter(iconID) {
+  if (document.getElementById(iconID).className == "fa-solid fa-filter") {
+    document.getElementById(iconID).className = "fa-solid fa-square-minus";
+    filterIcon.style.display = "inline-block";
+  } else {
+    document.getElementById(iconID).className = "fa-solid fa-filter";
+    filterIcon.style.display = "none";
+  }
+}
+
+//making the clear button functional
+clearBtn.addEventListener("click", clearFilters);
+
+//clear selected filters button
+function clearFilters() {
+  console.log(checkBoxes);
+  for (let i = 0; i < checkBoxes.length; i++) {
+    if (checkBoxes[i].type == "checkbox") {
+      checkBoxes[i].checked = false;
+    }
+  }
+}
+
+//searching
+searchBtn.addEventListener("click", search);
+
+function search() {
+  var searchInput = document.getElementById("search-input").value;
+  var result = 0;
+
+  jobContainer.innerHTML = "";
+
+  for (let i = 0, suffix = 1; i < jobArray.length; i++, suffix++) {
+    if (jobArray[i].title.toLowerCase().includes(searchInput.toLowerCase())) {
+      //counting results
+      result++;
+
+      jobContainer.innerHTML +=
+        `<div class="jobs" id="${jobArray[i].id}">
+              <div class="title-block">
+                <h3 onclick="openApplyJob('68%', ${jobArray[i].index})">
+                  ${jobArray[i].title}
+                </h3>
+                <div class="bookmark" onclick="saveJobIcon('bookmark-icon${suffix}')">
+                  <i class="fa-regular fa-bookmark" id="bookmark-icon${suffix}"></i>
+                </div>
+              </div>
+
+              <div class="job-info">
+                <span>
+                  Payment:
+                  <span>${jobArray[i].payment}ETB</span>
+                  - <span>${jobArray[i].experience}</span>
+                  - Est. Time:
+                  <span>2 to 5 months</span>
+                  - Posted -
+                  <span>${jobArray[i].date}</span>
+                </span>
+              </div>
+
+              <div class="description" onclick="openApplyJob('68%', ${jobArray[i].index})">
+                <p>
+                  ${jobArray[i].description}
+                </p>
+              </div> ` +
+        getTag(i) +
+        `<div class="apply-info">
+                <div class="text">Proposals: <span>${jobArray[i].proposals}</span></div>
+                <div class="text">Number of tokens: <span>${jobArray[i].token}</span></div>
+              </div>
+
+              <hr />
+            </div>`;
+    }
+  }
+  if (result == 0) {
+    jobContainer.innerHTML = '<p align="center" > No results were found';
+  }
+  resultLabel.innerHTML = result;
+}
