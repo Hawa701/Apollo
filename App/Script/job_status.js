e5ec1dac6645b3f0ccdc1583e85462660508f2c2
Mckali-2k <kalid538598@gmail.com>
2023-05-23 12:19:42 +0300

    updated job status

--- App/Script/job_status.js
+++ App/Script/job_status.js
@@ -4,8 +4,12 @@ function openJobStatus() {
     document.getElementById("main-section").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
   }
   
-  function closeJobStatus() {
+function closeJobStatus() {
     document.getElementById("job-status").style.width = "0%";
     document.getElementById("job-status").style.left = "100%";
     document.getElementById("main-section").style.backgroundColor = "#fff";
+  }
+
+  window.onload = function(){
+    closeJobStatus();
   }
