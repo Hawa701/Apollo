9105cfbe4b297e999d5a38e9ac8a845942e74399
Eyuel Solomon <122565078+Fine-Guy-21@users.noreply.github.com>
2023-05-31 14:43:58 +0300

    Updated login.js

--- App/Script/login.js
+++ App/Script/login.js
@@ -1,14 +1,15 @@
-
-
-
-
-
-
     const texts = document.querySelector('#texts');
     const password = document.querySelector('#password');
+    const backbtn = document.querySelector('#back-btn');
     const loginbtn = document.querySelector('#loginbtn');
     let emailpattern = new RegExp('[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,3}');// email pattern that uses small & capital letter & . & @
-    
+
+
+    backbtn.addEventListener('click', function(){
+        window.history.back();
+    })
+
+
     
 /*     
 loginbtn.addEventListener('click', function(e) {
