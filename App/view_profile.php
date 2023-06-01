5893030dea52ecefe6d65c716eecae6de0a6cdfb
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2023-06-01 23:58:55 +0300

    View Profile works
    Not responsive though

--- App/view_profile.php
+++ App/view_profile.php
@@ -1,15 +1,10 @@
 <?php
 
-session_start();
-
-// Check if the user is logged in
-if (isset($_SESSION['Profile_ID']) && $_SESSION['Profile_ID']) {
-  $profileId = $_SESSION['Profile_ID'];
-} else {
-  // User is not logged in, redirect to login page
-  header('Location: Login.php');
-// Exit the script to prevent further execution
-  exit();
+$current_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$url_parts = parse_url($current_url);
+if (isset($url_parts['query'])) {
+  parse_str($url_parts['query'], $query);
+  $profileId = $query['Profile_ID'];
 }
 
 include('Connect.php');
@@ -21,31 +16,30 @@ if (!$connect) {
 }
 
 // <span class="token" id="current-tokens">10</span>
-function getToken($connect, $profileId) {
-      $query = "SELECT profile.token FROM profile 
+function getToken($connect, $profileId)
+{
+  $query = "SELECT profile.token FROM profile 
                 WHERE profile.Profile_ID = $profileId";
 
-      $result = mysqli_query($connect, $query);
-      if($result && mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        echo "<span class='token' id='current-tokens'>{$row['token']}</span>"; 
-      } else{
-        echo "<span class='token' id='current-tokens'>Error!</span>"; 
-      }
-
+  $result = mysqli_query($connect, $query);
+  if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    echo "<span class='token' id='current-tokens'>{$row['token']}</span>";
+  } else {
+    echo "<span class='token' id='current-tokens'>Error!</span>";
+  }
 }
 
-
-
- function viewProfile($connect, $profileId) {
-    $query = "SELECT  profile.Firstname, profile.Lastname, profile.Username, profile.Email, profile.Experience_Level, profile.PricePerHour, profile.Profession, profile.Country  FROM profile
+function viewProfile($connect, $profileId)
+{
+  $query = "SELECT  profile.Firstname, profile.Lastname, profile.Username, profile.Email, profile.Experience_Level, profile.PricePerHour, profile.Profession, profile.Country  FROM profile
               WHERE profile.Profile_ID = $profileId";
 
-         $result = mysqli_query($connect, $query);
+  $result = mysqli_query($connect, $query);
 
-         if($result && mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            echo "
+  if ($result && mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    echo "
                 <h3>View Profile</h3>
 
                 <label for='firstname'>First Name</label>
@@ -72,77 +66,81 @@ function getToken($connect, $profileId) {
                 <label for='country'>Country</label>
                 <div class='country readOnly' id='country'> {$row['Country']} </div>               
             ";
-         } else {
-            echo "Error!";
-         }          
- }
-
- function updateProfile($connect, $profileId) {
-    // Get input values
-    $firstname = $_POST['firstname'] ?? null; 
-    $lastname = $_POST['lastname'] ?? null; 
-    $username = $_POST['username'] ?? null;
-    $email = $_POST['email'] ?? null;
-    $experience = $_POST['experience'] ?? null;
-    $priceperhour = $_POST['priceperhour'] ?? null;
-    $profession = $_POST['profession'] ?? null;
-    $country = $_POST['country'] ?? null;
-
-    // Build query string
-    $query = "UPDATE profile SET ";
-    $updates = [];
-
-    if (isset($firstname) && !empty($firstname)) {
-        $updates[] = "Firstname='$firstname'";
-    }
-    if (isset($lastname) && !empty($lastname)) {
-        $updates[] = "Lastname='$lastname'"; 
-    }
-    if (isset($username) && !empty($username)) {
-        $updates[] = "Username='$username'"; 
-    }
-    if (isset($email) && !empty($email)) {
-        $updates[] = "Email='$email'"; 
-    }
-    if (isset($experience) && !empty($experience)) {
-        $updates[] = "Experience_Level='$experience'"; 
-    }
-    if (isset($priceperhour) && !empty($priceperhour)) {
-        $updates[] = "PricePerHour='$priceperhour'"; 
-    }
-    if (isset($profession) &&!empty($profession)) {
-        $updates[] = "Profession='$profession'"; 
-    }
-    if (isset($country) && !empty($country)) {
-        $updates[] = "Country='$country'"; 
-    }
-
-    $query .= implode(', ', $updates);
-    $query .= " WHERE profile.Profile_ID = $profileId";
-
-    if (mysqli_query($connect, $query)) {
-        echo "Profile updated successfully";
-   } else {
-        echo "Error updating profile: " . mysqli_error($connect);
-    } 
+  } else {
+    echo "Error!";
+  }
 }
 
- // calling the updateProfile when a user cliks save 
- if (isset($_POST['save-btn'])) {
-    updateProfile($connect, $profileId);
+function updateProfile($connect, $profileId)
+{
+  // Get input values
+  $firstname = $_POST['firstname'] ?? null;
+  $lastname = $_POST['lastname'] ?? null;
+  $username = $_POST['username'] ?? null;
+  $email = $_POST['email'] ?? null;
+  $experience = $_POST['experience'] ?? null;
+  $priceperhour = $_POST['priceperhour'] ?? null;
+  $profession = $_POST['profession'] ?? null;
+  $country = $_POST['country'] ?? null;
+
+  // Build query string
+  $query = "UPDATE profile SET ";
+  $updates = [];
+
+  if (isset($firstname) && !empty($firstname)) {
+    $updates[] = "Firstname='$firstname'";
   }
+  if (isset($lastname) && !empty($lastname)) {
+    $updates[] = "Lastname='$lastname'";
+  }
+  if (isset($username) && !empty($username)) {
+    $updates[] = "Username='$username'";
+  }
+  if (isset($email) && !empty($email)) {
+    $updates[] = "Email='$email'";
+  }
+  if (isset($experience) && !empty($experience)) {
+    $updates[] = "Experience_Level='$experience'";
+  }
+  if (isset($priceperhour) && !empty($priceperhour)) {
+    $updates[] = "PricePerHour='$priceperhour'";
+  }
+  if (isset($profession) && !empty($profession)) {
+    $updates[] = "Profession='$profession'";
+  }
+  if (isset($country) && !empty($country)) {
+    $updates[] = "Country='$country'";
+  }
+
+  $query .= implode(', ', $updates);
+  $query .= " WHERE profile.Profile_ID = $profileId";
+
+  if (mysqli_query($connect, $query)) {
+    echo "<script>
+          alert(\"Profile Updated successfully!\");
+        </script>";
+  } else {
+    echo "Error updating profile: " . mysqli_error($connect);
+  }
+}
+
+// calling the updateProfile when a user cliks save 
+if (isset($_POST['save-btn'])) {
+  updateProfile($connect, $profileId);
+}
 
 
 //   when the user cliks buy tokn it will go to the add token page
 
 if (isset($_POST['buy-btn'])) {
-    header('Location: Add-tokens.php');
+  header('Location: Add-tokens.php');
 }
 
 // when the user clicks view posted jobs it will go to posted job page
 
 if (isset($_POST['posted-jobs'])) {
-    header('Location: PostedJobs.php');
+  $url = "PostedJobs.php?Profile_ID=" . urlencode($profileId);
+  header('Location:' . $url);
 }
 
 
@@ -150,69 +148,37 @@ if (isset($_POST['posted-jobs'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- font link -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
-      rel="stylesheet"
-    />
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- font link -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet" />
 
 
-    <!-- box icons -->
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <!-- box icons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-    <!-- Header css link -->
-    <link rel="stylesheet" href="./Style/Header.css" /> 
+  <!-- Header css link -->
+  <link rel="stylesheet" href="./Style/Header.css" />
 
-    <!-- css link -->
-    <link rel="stylesheet" href="./Style/view_profile.css?v=1.0" />
-    <!-- icon link -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <title>View Profile</title>
-  </head>
-  <body>
+  <!-- css link -->
+  <link rel="stylesheet" href="./Style/view_profile.css?v=1.3" />
+  <!-- icon link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>View Profile</title>
+</head>
 
-    <div class="wrapper">
+<body>
 
-       <!-- header design -->
-       <header class="header" id="header">
-        <div class="logo">
-            <a href="#">Apollo</a>
-        </div>
+  <div class="wrapper">
 
-        <div class="nav">
-            <ul class="links">
-                <li><a href="#">Find Work</a></li>
-                <li><a href="#">My Job</a></li>
-                <li><a href="#">Post a Job</a></li>
-                <li><a href="#">How it Works</a></li>
-                <!-- <li><a href="#">Message</a></li> -->
-            </ul>
-            <!-- <div class="notification">
-                <a href="#"><i class="fa-regular fa-bell"></i></a>
-            </div> -->
-        </div>
-
-        <div class="signInnUp">
-            <button class="login">Log In</button>
-            <button class="signup">Sign Up</button>
-        </div>
-        <!-- 
-        <div class="profile">
-            <a href="#"> H </a>
-        </div> -->
-    </header> <!-- header end -->
+    <?php
+    include('header.php');
+    ?>
 
 
     <!-- Section design -->
@@ -223,76 +189,76 @@ if (isset($_POST['posted-jobs'])) {
 
         <form class="edit-form" method="post">
 
-        <div class='left-side'>
+          <div class='left-side'>
 
-                <?php
-                viewProfile($connect, $profileId);
-                ?>
+            <?php
+            viewProfile($connect, $profileId);
+            ?>
 
-        </div> <!-- left side end -->
+          </div> <!-- left side end -->
 
           <div class="middle-side">
 
-                <h3>Update Profile</h3>
-                <div class="fullname">
-                    <div class="fname">
-                        <label for="update-firstname">Update First Name</label>
-                        <input type="text" name="firstname" id="update-firstname">
-                    </div>
+            <h3>Update Profile</h3>
+            <div class="fullname">
+              <div class="fname">
+                <label for="update-firstname">Update First Name</label>
+                <input type="text" name="firstname" id="update-firstname">
+              </div>
 
-                    <div class="lname">
-                        <label for="update-lastname">Update Last Name</label>
-                        <input type="text" name="lastname" id="update-lastname">
-                    </div>
-                </div>
+              <div class="lname">
+                <label for="update-lastname">Update Last Name</label>
+                <input type="text" name="lastname" id="update-lastname">
+              </div>
+            </div>
 
-                <label for="update-username">Update User Name</label>
-                <input type="text" name="username" id="update-username">
+            <label for="update-username">Update User Name</label>
+            <input type="text" name="username" id="update-username">
 
-                <label for="update-email">Update Email</label>
-                <input type="text" name="email" id="update-email">
+            <label for="update-email">Update Email</label>
+            <input type="text" name="email" id="update-email">
 
-                <label for="update-profession">Update Profession</label>
-                <input type="text" name="profession" id="update-profession"> 
+            <label for="update-profession">Update Profession</label>
+            <input type="text" name="profession" id="update-profession">
 
-                <label for="update-experience">Update Experience Level</label>
-                <select name="experience" id="update-experience">
-                    <option value="beginner">Beginner</option>
-                    <option value="intermediate">Intermediate</option>
-                    <option value="expert">Expert</option>
-                </select>
+            <label for="update-experience">Update Experience Level</label>
+            <select name="experience" id="update-experience">
+              <option value="beginner">Beginner</option>
+              <option value="intermediate">Intermediate</option>
+              <option value="expert">Expert</option>
+            </select>
 
-                <label for="update-priceperhour">Update Price Per Hour</label>
-                <input type="text" name="priceperhour" id="update-priceperhour">
+            <label for="update-priceperhour">Update Price Per Hour</label>
+            <input type="text" name="priceperhour" id="update-priceperhour">
 
-                <label for="update-country">Update Your Country</label>
-                <select name="country" id="update-country" class="update-country">
-                    <option value="Ethiopia" selected>Ethiopia</option>
-                    <option value="Kenya">Kenya</option>
-                    <option value="Eritrea">Eritrea</option>
-                    <option value="Somalia">Somalia</option>
-                    <option value="Djibouti">Djibouti</option>
-                    <option value="Sudan">Sudan</option>
-                <option value="South Sudan">South Sudan</option>
-                </select>
+            <label for="update-country">Update Your Country</label>
+            <select name="country" id="update-country" class="update-country">
+              <option value="Ethiopia" selected>Ethiopia</option>
+              <option value="Kenya">Kenya</option>
+              <option value="Eritrea">Eritrea</option>
+              <option value="Somalia">Somalia</option>
+              <option value="Djibouti">Djibouti</option>
+              <option value="Sudan">Sudan</option>
+              <option value="South Sudan">South Sudan</option>
+            </select>
 
-                <div class="save">
-                    <button class="save-btn" name="save-btn">Save</button>
-                </div>
+            <div class="save">
+              <button class="save-btn" name="save-btn">Save</button>
+            </div>
           </div> <!-- middle side end -->
 
           <div class="right-side">
-                <label for="current-tokens" class="current-tokens">Current Token: 
-                    <?php
-                    getToken($connect, $profileId);
-                    ?>
-                </label>
-            
-                <button class="buy-btn" name="buy-btn">Buy Token</button>
-    
-                <label for="posted-jobs">View Your Posted Jobs</label>
-                <button class="posted-jobs" name="posted-jobs">Posted Jobs</button>
-    
+            <label for="current-tokens" class="current-tokens">Current Token:
+              <?php
+              getToken($connect, $profileId);
+              ?>
+            </label>
+
+            <button class="buy-btn" name="buy-btn">Buy Token</button>
+
+            <label for="posted-jobs">View Your Posted Jobs</label>
+            <button class="posted-jobs" name="posted-jobs">Posted Jobs</button>
+
           </div> <!-- right side end -->
 
         </form> <!-- form end -->
@@ -301,9 +267,10 @@ if (isset($_POST['posted-jobs'])) {
 
     </section> <!-- section end -->
 
-    </div> <!-- wrapper end -->
-  
-     <!-- js link -->
-     <script src="./Script/view_profile.js"></script>
+  </div> <!-- wrapper end -->
+
+  <!-- js link -->
+  <script src="./Script/view_profile.js"></script>
 </body>
+
 </html>
